9bdb4225a0f337eba7306ebc09e7ac5b78793381
Corentin "Koroh" RICHARD <corentin.richard@etu.uca.fr>
2023-03-07 23:29:44 +0100

    fix stub

--- data/stub.ts
+++ data/stub.ts
@@ -4,18 +4,7 @@ export class StubLib {
     
     public getCards(): Card[] {
         const NOUNOURS_LIST : Card[] = [
-            new Card("EX1_572","Ysera","Classic","Minion","Neutral","Legendary",9,4,12,"","","",true,true,"Dragon","http://wow.zamimg.com/images/hearthstone/cards/enus/original/EX1_572.png","http://wow.zamimg.com/images/hearthstone/cards/enus/animated/EX1_572_premium.gif"),
-            new Card("EX1_573","A","Classic","Minion","Neutral","Legendary",9,4,12,"","","",true,true,"Dragon","http://wow.zamimg.com/images/hearthstone/cards/enus/original/EX1_573.png","http://wow.zamimg.com/images/hearthstone/cards/enus/animated/EX1_573_premium.gif"),
-            new Card("EX1_570","B","Classic","Minion","Neutral","Legendary",9,4,12,"","","",true,true,"Dragon","http://wow.zamimg.com/images/hearthstone/cards/enus/original/EX1_570.png","http://wow.zamimg.com/images/hearthstone/cards/enus/animated/EX1_570_premium.gif"),
-            new Card("EX1_575","C","Classic","Minion","Neutral","Legendary",9,4,12,"","","",true,true,"Dragon","http://wow.zamimg.com/images/hearthstone/cards/enus/original/EX1_575.png","http://wow.zamimg.com/images/hearthstone/cards/enus/animated/EX1_575_premium.gif"),
-            new Card("EX1_550","Ysera","Classic","Minion","Neutral","Legendary",9,4,12,"","","",true,true,"Dragon","http://wow.zamimg.com/images/hearthstone/cards/enus/original/EX1_572.png","http://wow.zamimg.com/images/hearthstone/cards/enus/animated/EX1_572_premium.gif"),
-            new Card("EX1_563","A","Classic","Minion","Neutral","Legendary",9,4,12,"","","",true,true,"Dragon","http://wow.zamimg.com/images/hearthstone/cards/enus/original/EX1_573.png","http://wow.zamimg.com/images/hearthstone/cards/enus/animated/EX1_573_premium.gif"),
-            new Card("EX1_580","B","Classic","Minion","Neutral","Legendary",9,4,12,"","","",true,true,"Dragon","http://wow.zamimg.com/images/hearthstone/cards/enus/original/EX1_570.png","http://wow.zamimg.com/images/hearthstone/cards/enus/animated/EX1_570_premium.gif"),
-            new Card("EX1_595","C","Classic","Minion","Neutral","Legendary",9,4,12,"","","",true,true,"Dragon","http://wow.zamimg.com/images/hearthstone/cards/enus/original/EX1_575.png","http://wow.zamimg.com/images/hearthstone/cards/enus/animated/EX1_575_premium.gif"),
-            new Card("EX1_542","Ysera","Classic","Minion","Neutral","Legendary",9,4,12,"","","",true,true,"Dragon","http://wow.zamimg.com/images/hearthstone/cards/enus/original/EX1_572.png","http://wow.zamimg.com/images/hearthstone/cards/enus/animated/EX1_572_premium.gif"),
-            new Card("EX1_533","A","Classic","Minion","Neutral","Legendary",9,4,12,"","","",true,true,"Dragon","http://wow.zamimg.com/images/hearthstone/cards/enus/original/EX1_573.png","http://wow.zamimg.com/images/hearthstone/cards/enus/animated/EX1_573_premium.gif"),
-            new Card("EX1_520","B","Classic","Minion","Neutral","Legendary",9,4,12,"","","",true,true,"Dragon","http://wow.zamimg.com/images/hearthstone/cards/enus/original/EX1_570.png","http://wow.zamimg.com/images/hearthstone/cards/enus/animated/EX1_570_premium.gif"),
-            new Card("EX1_515","C","Classic","Minion","Neutral","Legendary",9,4,12,"","","",true,true,"Dragon","http://wow.zamimg.com/images/hearthstone/cards/enus/original/EX1_575.png","http://wow.zamimg.com/images/hearthstone/cards/enus/animated/EX1_575_premium.gif"),
+            
             
         ]
         return NOUNOURS_LIST
