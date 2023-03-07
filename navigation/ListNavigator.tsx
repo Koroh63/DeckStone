61bb443e1474aabacb520aa34de035ae61a04642
Corentin "Koroh" RICHARD <corentin.richard@etu.uca.fr>
2023-03-07 23:24:54 +0100

    Adding new basic structure for API

--- navigation/ListNavigator.tsx
+++ navigation/ListNavigator.tsx
@@ -1,21 +1,21 @@
 
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
 
-import HomeScreen from '../screens/HomeScreen';
-import ListScreen from '../screens/ListScreen';
-import ListFav from '../screens/ListFav';
-
-
+// import HomeScreen from '../screens/HomeScreen';
+// import ListScreen from '../screens/ListScreen';
+// import ListFav from '../screens/ListFav';
 
 
-export default function ListNavigator() {
-    const Stack = createStackNavigator();
-    return (
-        <Stack.Navigator initialRouteName="List">
-        <Stack.Screen name="List" component={ListScreen}/>
-        <Stack.Screen name="ListFav" component={ListFav}/>
-        </Stack.Navigator>
-    )
-}
+
+
+// export default function ListNavigator() {
+//     const Stack = createStackNavigator();
+//     return (
+//         <Stack.Navigator initialRouteName="List">
+//         <Stack.Screen name="List" component={ListScreen}/>
+//         <Stack.Screen name="ListFav" component={ListFav}/>
+//         </Stack.Navigator>
+//     )
+// }
--- screens/ListFav.tsx
+++ screens/ListFav.tsx
@@ -25,7 +25,5 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
-        borderWidth: 5,
-        borderColor : "#cacaca",
     },
 });
